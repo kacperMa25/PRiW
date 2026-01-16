1a2008837fd5fc6975a27ed6a29611bcb2502d0e
kacper <mk55552@zut.edu.pl>
2026-01-16 15:25:00 +0100

    lab08 - mod

--- LAB08/main.cpp
+++ LAB08/main.cpp
@@ -1,9 +1,12 @@
 #define CL_TARGET_OPENCL_VERSION 120
 
 #include <OpenCL/cl.h>
+#include <chrono>
 #include <cstring>
 #include <iostream>
 #include <vector>
+
+#define MAX_SIZE 25600
 
 char* readKernelSource(const char* filename, size_t* length)
 {
@@ -28,61 +31,75 @@ char* readKernelSource(const char* filename, size_t* length)
 int main()
 {
     // Inicjalizacja danych macierzy
-    const unsigned int M = 6400, N = 6400, K = 6400;
-    std::vector<float> A(N * N, 1.0f); // Macierz A
-    std::vector<float> B(N * N, 1.0f); // Macierz B
-    std::vector<float> C(N * N, 0.0f); // Macierz wynikowa C
-    // Inicjalizacja OpenCL
-    cl_int err;
-    cl_platform_id platform;
-    clGetPlatformIDs(1, &platform, nullptr);
-    cl_device_id device;
-    clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, nullptr);
-    cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &err);
-    cl_command_queue queue = clCreateCommandQueue(context, device, 0, &err);
-    // Alokacja pamiÄci na dane
-    cl_mem bufferA = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N * N, A.data(), &err);
-    cl_mem bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N * N, B.data(), &err);
-    cl_mem bufferC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * N * N, nullptr, &err);
-    // ZaĹadowanie kodu kernelu
-    size_t sourceSize;
-    const char* kernelSource = readKernelSource("kernel.cl", &sourceSize);
-    cl_program program = clCreateProgramWithSource(context, 1, &kernelSource, &sourceSize, &err);
-    clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
-    if (err != CL_SUCCESS) {
-        // ObsĹuga bĹÄdu
-        size_t logSize;
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &logSize);
-        std::vector<char> buildLog(logSize);
-        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, buildLog.data(), nullptr);
-        std::cerr << "Build Error:" << std::endl;
-        std::cerr << buildLog.data() << std::endl;
+    for (int i = 1200; i <= MAX_SIZE; i *= 2) {
+
+        auto start = std::chrono::high_resolution_clock::now();
+        const unsigned int M = 6400, N = i, K = 6400;
+        std::vector<float> A(N * N, 1.0f); // Macierz A
+        std::vector<float> B(N * N, 1.0f); // Macierz B
+        std::vector<float> C(N * N, 0.0f); // Macierz wynikowa C
+
+        // Inicjalizacja OpenCL
+        cl_int err;
+        cl_platform_id platform;
+        clGetPlatformIDs(1, &platform, nullptr);
+        cl_device_id device;
+        clGetDeviceIDs(platform, CL_DEVICE_TYPE_GPU, 1, &device, nullptr);
+        cl_context context = clCreateContext(nullptr, 1, &device, nullptr, nullptr, &err);
+        cl_command_queue queue = clCreateCommandQueue(context, device, 0, &err);
+
+        // Alokacja pamiÄci na dane
+        cl_mem bufferA = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N * N, A.data(), &err);
+        cl_mem bufferB = clCreateBuffer(context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(float) * N * N, B.data(), &err);
+        cl_mem bufferC = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * N * N, nullptr, &err);
+
+        // ZaĹadowanie kodu kernelu
+        size_t sourceSize;
+        const char* kernelSource = readKernelSource("kernel.cl", &sourceSize);
+        cl_program program = clCreateProgramWithSource(context, 1, &kernelSource, &sourceSize, &err);
+        clBuildProgram(program, 1, &device, nullptr, nullptr, nullptr);
+        if (err != CL_SUCCESS) {
+            // ObsĹuga bĹÄdu
+            size_t logSize;
+            clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, 0, nullptr, &logSize);
+            std::vector<char> buildLog(logSize);
+            clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, logSize, buildLog.data(), nullptr);
+            std::cerr << "Build Error:" << std::endl;
+            std::cerr << buildLog.data() << std::endl;
+        }
+
+        cl_kernel kernel = clCreateKernel(program, "matrix_multiply_2d", &err);
+
+        // Przekazanie danych do kernela
+        clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufferA);
+        clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufferB);
+        clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufferC);
+        clSetKernelArg(kernel, 3, sizeof(unsigned int), &N);
+
+        // Ustawienie rozmiaru grupy roboczej i globalnego rozmiaru
+        size_t globalSize[] = { M, N };
+        size_t localSize[] = { 16, 16 }; // Wymiary grupy roboczej
+
+        // Uruchomienie kernela
+        err = clEnqueueNDRangeKernel(queue, kernel, 2, nullptr, globalSize, localSize, 0, nullptr, nullptr);
+        clFinish(queue);
+
+        // Pobranie wynikĂłw
+        err = clEnqueueReadBuffer(queue, bufferC, CL_TRUE, 0, sizeof(float) * M * N, C.data(), 0, nullptr, nullptr);
+
+        // Czyszczenie zasobĂłw
+        clReleaseMemObject(bufferA);
+        clReleaseMemObject(bufferB);
+        clReleaseMemObject(bufferC);
+        clReleaseKernel(kernel);
+        clReleaseProgram(program);
+        clReleaseCommandQueue(queue);
+        clReleaseContext(context);
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsedTime { end - start };
+
+        std::cout << "Size of N = " << N << std::endl;
+        std::cout << "Time: " << elapsedTime.count() << " s" << std::endl;
     }
-
-    cl_kernel kernel = clCreateKernel(program, "matrix_multiply_2d", &err);
-    // Przekazanie danych do kernela
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), &bufferA);
-    clSetKernelArg(kernel, 1, sizeof(cl_mem), &bufferB);
-    clSetKernelArg(kernel, 2, sizeof(cl_mem), &bufferC);
-    clSetKernelArg(kernel, 3, sizeof(unsigned int), &N);
-
-    // Ustawienie rozmiaru grupy roboczej i globalnego rozmiaru
-    size_t globalSize[] = { M, N };
-    size_t localSize[] = { 16, 16 }; // Wymiary grupy roboczej
-    // Uruchomienie kernela
-    err = clEnqueueNDRangeKernel(queue, kernel, 2, nullptr, globalSize, localSize, 0, nullptr, nullptr);
-    clFinish(queue);
-    // Pobranie wynikĂłw
-    err = clEnqueueReadBuffer(queue, bufferC, CL_TRUE, 0, sizeof(float) * M * N, C.data(), 0, nullptr, nullptr);
-    // WyĹwietlenie wynikĂłw
-    std::cout << "C[0] = " << C[0] << std::endl;
-    // Czyszczenie zasobĂłw
-    clReleaseMemObject(bufferA);
-    clReleaseMemObject(bufferB);
-    clReleaseMemObject(bufferC);
-    clReleaseKernel(kernel);
-    clReleaseProgram(program);
-    clReleaseCommandQueue(queue);
-    clReleaseContext(context);
     return 0;
 }
